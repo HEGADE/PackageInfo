9d420bafc7c118485c88c0efb15aaa442920c78e
HEGADE <subramanyashegade@gmail.com>
2021-12-31 22:53:18 +0530

    fixed the bug which was causing rerendering

--- src/select.js
+++ src/select.js
@@ -1,47 +1,58 @@
 import { showPopUp } from "./showInfo";
 console.log("successfully added the script");
-window.onload=()=>{
 
-  
-    // let data = await fetch("https://api.github.com/users");
-    // let jsonData = await data.json();
-    showPopUp(111,"Dd")
-  
-    let selection=document.querySelector(".id-22-lol section")
-    let div=document.querySelector(".id-22-lol")
-    document.addEventListener('selectionchange', (e) => {
-      console.log(e)
-    let selectedText=window.getSelection().toString();
-    console.log(selectedText)
-    if(selectedText){
-      console.log(`"${window.getSelection().focusOffset*5}px"`)
-      selection.style.display="block";
-      console.log(document.querySelector("body").getBoundingClientRect())
-      let bodyAxis=document.querySelector("body").getBoundingClientRect()
-      console.log(document.querySelector("body"))
-      let top=`${bodyAxis.top-1000}px`
-      let left=`${bodyAxis.left-1000}px`
-      let right=`${bodyAxis.right-1000}px`
-      let bottom=`${bodyAxis.bottom-500}px`
-      // let top=`${bodyAxis.top}px`
-    // selection.style.marginTop=bottom
-    // selection.style.marginLeft=left
-    // selection.style.marginRight=right
-    // selection.style.marginBottom=bo
+const getData = async (searchPackage) => {
+  let data = await fetch(`https://api.npms.io/v2/search?q=${searchPackage}`);
+  let jsonData = await data.json();
+  return jsonData;
+};
+const sleep = (time)=>
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve();
+  }, time);
+});
+let prevText=null
+window.onload = () => {
 
-    
+  showPopUp(111, "sd");
+  let selection = document.querySelector(".id-22-lol section");
+  let div = document.querySelector(".id-22-lol");
+  let renderCount = 0;
+  document.addEventListener("selectionchange", async (e) => {
+
+    await sleep(1000);
+    let selectedText = window.getSelection().toString();
+    console.log("after something");
+    renderCount++;
+    // let selectedText = null;          
+
+    console.log(selectedText);
+    if (selectedText&&selectedText!=prevText) {
+      
+       
+      console.log(selectedText,prevText,selectedText!=prevText);
+      prevText=selectedText
+        let data = await getData(selectedText);
+        console.log(data?.results[0]?.package?.description)
+
+       
+
+      selection.style.display = "block";
+
+      renderCount=0
+      return
     }
+  });
     // document.onmousedown
-    document.addEventListener('click', function(e) {
-   
+    document.addEventListener("click", function (e) {
       let left = e.offsetX;
       let top = e.offsetY;
-      console.log(left,top,window.scrollX,window.scrollY,)
-      selection.style.marginLeft = left-top + 'px';
-      let topp=`${Math.abs(window.scrollY-top)}px`
-      console.log(topp)
-      selection.style.marginTop =topp
-    })
-    
+      console.log(left, top, window.scrollX, window.scrollY);
+      selection.style.marginLeft = left - top + "px";
+      let topp = `${Math.abs(window.scrollY - top)}px`;
+      console.log(topp);
+      selection.style.marginTop = topp;
     });
-}
+  
+};
--- src/showInfo.js
+++ src/showInfo.js
@@ -1,4 +1,5 @@
 export let showPopUp=(pointerPosition,data)=>{
+  console.log(data)
     let body=document.querySelector("body")
     let div=document.createElement("div")
     div.className="id-22-lol"
