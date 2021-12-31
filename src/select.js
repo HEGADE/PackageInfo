4190c525869fb1cee03f499d98bc2957527aa57f
HEGADE <subramanyashegade@gmail.com>
2021-12-31 23:30:07 +0530

    done with app building

--- src/select.js
+++ src/select.js
@@ -6,53 +6,63 @@ const getData = async (searchPackage) => {
   let jsonData = await data.json();
   return jsonData;
 };
-const sleep = (time)=>
-new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve();
-  }, time);
-});
-let prevText=null
+const sleep = (time) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
+let prevText = null;
 window.onload = () => {
-
   showPopUp(111, "sd");
   let selection = document.querySelector(".id-22-lol section");
   let div = document.querySelector(".id-22-lol");
+  let h1 = document.querySelector("#h1-lol-123");
+  let p = document.querySelector("#p-lol-123");
+  let li1 = document.querySelector("#li1-lol-123");
+  let li2 = document.querySelector("#li2-lol-123");
+  let a1 = document.querySelector("#a1-lol-123");
+  let a2 = document.querySelector("#a2-lol-123");
   let renderCount = 0;
   document.addEventListener("selectionchange", async (e) => {
-
     await sleep(1000);
     let selectedText = window.getSelection().toString();
     console.log("after something");
     renderCount++;
-    // let selectedText = null;          
 
     console.log(selectedText);
-    if (selectedText&&selectedText!=prevText) {
-      
-       
-      console.log(selectedText,prevText,selectedText!=prevText);
-      prevText=selectedText
-        let data = await getData(selectedText);
-        console.log(data?.results[0]?.package?.description)
+    if (selectedText && selectedText != prevText) {
+      console.log(selectedText, prevText, selectedText != prevText);
+      prevText = selectedText;
+      let data = await getData(selectedText);
+      console.log(data?.results[0]?.package?.description);
+      h1.innerText=data?.results[0]?.package?.name
+      p.innerText = data?.results[0]?.package?.description;
+      li1.innerText =
+        "Publisher: " + data?.results[0]?.package?.publisher?.username;
+      li2.innerText = "Mail: " + data?.results[0]?.package?.publisher?.email;
+      a1.href = data?.results[0]?.package?.links?.npm;
+      if (data?.results[0]?.package?.links?.repository){
 
-       
+        a2.href = data?.results[0]?.package?.links?.repository;
+      }
+      else {a2.innerText = ""};
+      console.log(data?.results[0]?.package);
 
       selection.style.display = "block";
 
-      renderCount=0
-      return
+      renderCount = 0;
+      return;
     }
   });
-    // document.onmousedown
-    document.addEventListener("click", function (e) {
-      let left = e.offsetX;
-      let top = e.offsetY;
-      console.log(left, top, window.scrollX, window.scrollY);
-      selection.style.marginLeft = left - top + "px";
-      let topp = `${Math.abs(window.scrollY - top)}px`;
-      console.log(topp);
-      selection.style.marginTop = topp;
-    });
-  
+  // document.onmousedown
+  document.addEventListener("click", function (e) {
+    let left = e.offsetX;
+    let top = e.offsetY;
+    console.log(left, top, window.scrollX, window.scrollY);
+    selection.style.marginLeft = left - top + "px";
+    let topp = `${Math.abs(window.scrollY - top)}px`;
+    console.log(topp);
+    selection.style.marginTop = topp;
+  });
 };
--- src/showInfo.js
+++ src/showInfo.js
@@ -5,20 +5,20 @@ export let showPopUp=(pointerPosition,data)=>{
     div.className="id-22-lol"
     let innerContent=`
     <section style="display:none;" id="section-lol-123" >
-        <h1>Something_sos_ss_print</h1>
+        <h1 id="h1-lol-123">Something_sos_ss_print</h1>
         <br />
-        <p>
+        <p id="p-lol-123">
           lorem ipsum something is ninja and ninja is maja after all lorem ipsum
           something is ninja and ninja is maja after all
         </p>
         <ul>
-          <li>Author: ninja</li>
-          <li>Mail: ninja@manja.com</li>
+          <li id="li1-lol-123">Author: ninja</li>
+          <li id="li2-lol-123">Mail: ninja@manja.com</li>
         </ul>
 
         <div class="imge-container-12-lol">
-          <a href="htts">Npm</a>
-          <a href="htts">github</a>
+          <a id="a1-lol-123" href="#">Npm</a>
+          <a id="a2-lol-123" href="#">github</a>
         </div>
       </section>
     `
