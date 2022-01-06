2ecc39dbc7154faa71040dbe7a13870e3562ed2b
HEGADE <subramanyashegade@gmail.com>
2022-01-07 00:23:43 +0530

    fixed not found page logic

--- src/pypi/utils.js
+++ src/pypi/utils.js
@@ -1,12 +1,25 @@
-function makeReadmore(des,package_url) {
-  // console.log(des.length())
-  return des.length > 100 ? des.substring(0, 100) + "  ...... "+` <a href=${package_url} target="_blank">readmore</a>` : des ;
+function makeReadmore(des, package_url) {
+  return des.length > 100
+    ? des.substring(0, 100) +
+        "  ...... " +
+        ` <a href=${package_url} target="_blank">readmore</a>`
+    : des;
 }
 export const fillPopupForPython = (DOMEle, packagePyPi) => {
   console.log(packagePyPi?.name);
-  if (packagePyPi.author === "UNKNOWN") throw new Error("Not found");
+
+  if (packagePyPi?.author === "UNKNOWN" || !packagePyPi?.name) {
+    packagePyPi = {};
+
+    throw new Error("Not found");
+  }
   DOMEle.packageNpmName.innerText = packagePyPi.name;
-  DOMEle.packageNpmDes.innerHTML =   makeReadmore(packagePyPi.description,packagePyPi.package_url);
+  if (packagePyPi.description === "UNKNOWN")
+    packagePyPi.description = "Description is not available";
+
+  DOMEle.packageNpmDes.innerHTML =
+    makeReadmore(packagePyPi.description, packagePyPi.package_url) ||
+    "Not found";
   DOMEle.author.innerText = "Publisher: " + packagePyPi.author;
   DOMEle.authorMail.innerText = "Mail: " + packagePyPi.author_email;
   DOMEle.npmLink.href = packagePyPi.package_url;
@@ -18,5 +31,3 @@ export const fillPopupForPython = (DOMEle, packagePyPi) => {
     DOMEle.githubLink.innerText = "";
   }
 };
-
-
